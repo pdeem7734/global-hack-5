46e8bab7d3daf3b9790b46fcaedbc79636c8d203
Paul <pdeem_code@outlook.com>
2015-08-27 18:07:24 -0500

    Added project starting point.

---
+++ global-hack-5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(global_hack_5.Startup))]
+namespace global_hack_5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+        }
+    }
+}
