7083baebacac150db2275385ae2eed30ff4e0e3e
Paul <pdeem_code@outlook.com>
2015-09-10 23:12:57 -0500

    quick example rest service contacting external soap service (maybe a thing we end up doing

---
+++ globalhack-5/Controllers/PlayWithWebAPIController.cs
@@ -0,0 +1,20 @@
+﻿using globalhack_5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.ServiceModel;
+using System.Web.Http;
+
+namespace globalhack_5.Controllers
+{
+    public class PlayWithWebAPIController : ApiController
+    {
+        public TestResponseWithSomeThings Get(string id = null)
+        {
+            TestResponseWithSomeThings response = new TestResponseWithSomeThings();
+            return response;
+        }
+    }
+}
---
+++ globalhack-5/Models/TestResponseWithSomeThings.cs
@@ -0,0 +1,34 @@
+﻿using globalhack_5.GlobalWeather;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace globalhack_5.Models
+{
+    public class TestResponseWithSomeThings
+    {
+        private string _someValue = "This is a test value";
+
+        public TestResponseWithSomeThings()
+        {
+            BasicHttpBinding binding = new BasicHttpBinding();
+            EndpointAddress address = new EndpointAddress("http://www.webservicex.com/globalweather.asmx");
+            GlobalWeatherSoapClient soapClient = new GlobalWeatherSoapClient(binding, address);
+            _someValue = soapClient.GetWeather("Alton / St. Louis Regional", "United States");
+        }
+
+        public string someValue
+        {
+            get
+            {
+                return _someValue;
+            }
+            set
+            {
+                this._someValue = value ?? this._someValue;
+            }
+        }
+    }
+}
