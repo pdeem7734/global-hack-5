e834d49a2e5575e1bf8b37bb05a92a37fed687a3
Paul <pdeem7734@github.com>
2015-09-12 09:56:38 -0500

    I ADDED COURTS

---
+++ web-api/src/main/java/bison/solutions/api/Courts.java
@@ -0,0 +1,21 @@
+package bison.solutions.api;
+
+import bison.solutions.domain.Court;
+import bison.solutions.hazelcast.HazelcastConnection;
+
+import javax.ws.rs.*;
+
+@Path("Court")
+public class Courts {
+    HazelcastConnection hazelcastConnection;
+
+
+    @GET
+    @Consumes("*/*")
+    @Produces("application/json")
+    @Path("/Municipality/{municipality}")
+    public Court getCourtByMunicipality(@PathParam("municipality") String municipality) {
+        hazelcastConnection = HazelcastConnection.hazelcastConnection;
+        return (Court) hazelcastConnection.hazelcastInstance.getMap(hazelcastConnection.CourtNamespace).get(municipality);
+    }
+}
