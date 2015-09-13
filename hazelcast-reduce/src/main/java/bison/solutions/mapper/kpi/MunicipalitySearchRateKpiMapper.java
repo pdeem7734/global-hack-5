b996f4bffcc037edb3c7f60b4348bd30dba150c2
Josh <joshuagagen@gmail.com>
2015-09-12 20:34:07 -0500

    SearchRate StringStringWrapperFacade

--- hazelcast-reduce/src/main/java/bison/solutions/mapper/kpi/MunicipalitySearchRateKpiMapper.java
+++ hazelcast-reduce/src/main/java/bison/solutions/mapper/kpi/MunicipalitySearchRateKpiMapper.java
@@ -5,24 +5,45 @@ import com.hazelcast.mapreduce.Context;
 import com.hazelcast.mapreduce.Mapper;
 
 
-public class MunicipalitySearchRateKpiMapper implements Mapper<String, Feature, String, Feature> {
+public class MunicipalitySearchRateKpiMapper implements Mapper<String, Feature, String, StringStringWrapperFacade> {
     private String municipality;
     
     public MunicipalitySearchRateKpiMapper(String municipality) { this.municipality = municipality; }
 
     @Override
-    public void map(String s, Feature feature, Context<String, Feature> context) {
+    public void map(String s, Feature feature, Context<String, StringStringWrapperFacade> context) {
         if (feature.properties.court_name.equals(municipality)) {
 
-            Feature newFeature = new Feature();
-            newFeature.properties.asian_search_rate = feature.properties.asian_search_rate;
-            newFeature.properties.black_search_rate = feature.properties.black_search_rate;
-            newFeature.properties.indian_search_rate = feature.properties.indian_search_rate;
-            newFeature.properties.white_search_rate = feature.properties.white_search_rate;
-            newFeature.properties.hispanic_search_rate = feature.properties.hispanic_search_rate;
-            newFeature.properties.other_search_rate = feature.properties.other_search_rate;
+            StringStringWrapperFacade stringStringWrapperFacade = new StringStringWrapperFacade();
 
-            context.emit(municipality, newFeature);
+            stringStringWrapperFacade.thing0 = "Asian Search Rate";
+            stringStringWrapperFacade.thing1 = feature.properties.asian_search_rate;
+            context.emit(municipality, stringStringWrapperFacade);
+
+            stringStringWrapperFacade = new StringStringWrapperFacade();
+            stringStringWrapperFacade.thing0 = "Black Search Rate";
+            stringStringWrapperFacade.thing1 = feature.properties.black_search_rate;
+            context.emit(municipality, stringStringWrapperFacade);
+
+            stringStringWrapperFacade = new StringStringWrapperFacade();
+            stringStringWrapperFacade.thing0 = "Indian Search Rate";
+            stringStringWrapperFacade.thing1 = feature.properties.indian_search_rate;
+            context.emit(municipality, stringStringWrapperFacade);
+
+            stringStringWrapperFacade = new StringStringWrapperFacade();
+            stringStringWrapperFacade.thing0 = "White Search Rate";
+            stringStringWrapperFacade.thing1 = feature.properties.white_search_rate;
+            context.emit(municipality, stringStringWrapperFacade);
+
+            stringStringWrapperFacade = new StringStringWrapperFacade();
+            stringStringWrapperFacade.thing0 = "Hispanic Search Rate";
+            stringStringWrapperFacade.thing1 = feature.properties.hispanic_search_rate;
+            context.emit(municipality, stringStringWrapperFacade);
+
+            stringStringWrapperFacade = new StringStringWrapperFacade();
+            stringStringWrapperFacade.thing0 = "Other Search Rate";
+            stringStringWrapperFacade.thing1 = feature.properties.other_search_rate;
+            context.emit(municipality, stringStringWrapperFacade);
         }
     }
 }
--- web-api/src/main/java/bison/solutions/api/Kpi.java
+++ web-api/src/main/java/bison/solutions/api/Kpi.java
@@ -74,20 +74,20 @@ public class Kpi {
     @Consumes("*/*")
     @Produces("application/json")
     @Path("/Municipality/{municipality}/SearchRate")
-    public Feature getSearchRateDonut(String municipality) throws ExecutionException, InterruptedException {
+    public StringStringWrapperFacade getSearchRateDonut(String municipality) throws ExecutionException, InterruptedException {
         hazelcastConnection = HazelcastConnection.hazelcastConnection;
         JobTracker jobTracker = hazelcastConnection.hazelcastInstance.getJobTracker(hazelcastConnection.MuniReduceNamespace);
         ISet< Feature> bestestSet = hazelcastConnection.hazelcastInstance.getSet(hazelcastConnection.bigThingNamespace);
         KeyValueSource<String, Feature> source = KeyValueSource.fromSet(bestestSet);
         Job< String, Feature > jobs = jobTracker.newJob(source);
 
-        Map<String, List<Feature>> map = jobs.mapper(
+        Map<String, List<StringStringWrapperFacade>> map = jobs.mapper(
                 new MunicipalitySearchRateKpiMapper(municipality))
-                .reducer(new ListReducer<String, Feature>())
+                .reducer(new ListReducer<String, StringStringWrapperFacade>())
                 .submit().get();
 
-        List<Feature> returnMe = new LinkedList<>();
-        for (List<Feature> list : map.values()) {
+        List<StringStringWrapperFacade> returnMe = new LinkedList<>();
+        for (List<StringStringWrapperFacade> list : map.values()) {
             returnMe.addAll(list);
         }
         return returnMe.get(0);
