01670531046960f2714a07ef89985c4f6ac9bffb
Paul <pdeem7734@github.com>
2015-09-12 19:38:26 -0500

    POST

--- web-api/src/main/java/bison/solutions/api/Courts.java
+++ web-api/src/main/java/bison/solutions/api/Courts.java
@@ -3,6 +3,7 @@ package bison.solutions.api;
 import bison.solutions.domain.Court;
 import bison.solutions.hazelcast.HazelcastConnection;
 
+import javax.annotation.PostConstruct;
 import javax.ws.rs.*;
 
 @Path("Court")
@@ -10,11 +11,11 @@ public class Courts {
     HazelcastConnection hazelcastConnection;
 
 
-    @GET
+    @POST
     @Consumes("*/*")
     @Produces("application/json")
-    @Path("/Municipality/{municipality}")
-    public Court getCourtByMunicipality(@PathParam("municipality") String municipality) {
+    @Path("/Municipality")
+    public Court getCourtByMunicipality(String municipality) {
         hazelcastConnection = HazelcastConnection.hazelcastConnection;
         return (Court) hazelcastConnection.hazelcastInstance.getMap(hazelcastConnection.CourtNamespace).get(municipality.toUpperCase());
     }
